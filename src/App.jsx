e542832aa9e08325b301ec83d1469630fd6bae9a
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-09-13 12:07:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <h1 className="text-3xl font-bold underline">
+      
+    </h1>
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
